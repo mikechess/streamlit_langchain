fb70fd185ec2e90becaeb7de08e7df168bad2f31
mikechess <mikecook450@gmail.com>
2025-09-10 19:09:25 -0400

    Chat Functionality

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 import os
 import streamlit as st
 from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,6 +17,29 @@ st.set_page_config(page_title="Streaming Bot", page_icon="🤖")
 
 st.title("Streaming Bot")
 
+# get response
+def get_response(query, chat_history):
+    template = template = """
+        You are a helpful assistant. Answer the following questions considering the history of the conversation:
+
+        Chat history: {chat_history}
+
+        User question: {user_question}
+        """
+        
+    prompt = ChatPromptTemplate.from_template(template)
+    llm = ChatGoogleGenerativeAI(
+        model="models/gemini-2.0-flash",
+        api_key=os.environ.get("GEMNIN_API_KEY")
+    )    
+    chain = prompt | llm | StrOutputParser()
+    
+    return chain.invoke({
+        "chat_history": chat_history,
+        "user_question": query
+    })
+
+
 # Conversation
 for message in st.session_state.chat_history:
     if isinstance(message, HumanMessage):
@@ -22,6 +49,7 @@ for message in st.session_state.chat_history:
         with st.chat_message("AI"):
             st.markdown(message.content)
 
+# user input
 user_query = st.chat_input("Your message")
 if user_query is not None and user_query != "":
     st.session_state.chat_history.append(HumanMessage(user_query))
@@ -30,7 +58,7 @@ if user_query is not None and user_query != "":
         st.markdown(user_query)
         
     with st.chat_message("AI"):
-        ai_response = "I don't know"
+        ai_response = get_response(user_query, st.session_state.chat_history)
         st.markdown(ai_response)
         
     st.session_state.chat_history.append(AIMessage(ai_response))
